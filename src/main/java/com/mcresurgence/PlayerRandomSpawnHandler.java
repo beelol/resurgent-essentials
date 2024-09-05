199fd7ad4b1f1599b951ef2235b1b51b89326cca
Bilal Itani <bilalitani1@gmail.com>
2024-09-05 15:47:12 -0500

    Add random spawn

--- src/main/java/com/mcresurgence/PlayerRandomSpawnHandler.java
+++ src/main/java/com/mcresurgence/PlayerRandomSpawnHandler.java
@@ -1,9 +1,12 @@
 package com.mcresurgence;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
@@ -12,33 +15,35 @@ import java.util.Random;
 
 public class PlayerRandomSpawnHandler {
 
+    private boolean isRandomSpawnAllowed(EntityPlayerMP player) {
+        return ServerMOTDConfig.isRandomSpawnEnabled() && !hasValidBedSpawn(player);
+    }
+
     @SubscribeEvent
     public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
         if (!(event.player instanceof EntityPlayerMP)) return;
 
         EntityPlayerMP playerMP = (EntityPlayerMP) event.player;
-        NBTTagCompound playerData = playerMP.getEntityData();
+        NBTTagCompound persistentData = playerMP.getEntityData().getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
 
         // Check if this is the player's first join
-        if (!playerData.hasKey("HasSpawned")) {
-            if (ServerMOTDConfig.isRandomSpawnEnabled()) {
-                // Set the spawn point before the player is fully initialized in the world
+        if (!persistentData.getBoolean("HasSpawned")) {
+            if (isRandomSpawnAllowed(playerMP)) {
                 BlockPos randomSpawnPoint = getRandomSpawnPoint(playerMP.getEntityWorld(), playerMP.getPosition(), ServerMOTDConfig.getSpawnRadius());
-                playerMP.setSpawnPoint(randomSpawnPoint, true);  // Set the new spawn point for the player
 
-                playerMP.connection.setPlayerLocation(randomSpawnPoint.getX() + 0.5, randomSpawnPoint.getY(), randomSpawnPoint.getZ() + 0.5, playerMP.rotationYaw, playerMP.rotationPitch);
-
-                playerMP.attemptTeleport(randomSpawnPoint.getX() + 0.5, randomSpawnPoint.getY(), randomSpawnPoint.getZ() + 0.5);
-
+                // Immediately update the player's position
+                playerMP.setPositionAndUpdate(randomSpawnPoint.getX(), randomSpawnPoint.getY(), randomSpawnPoint.getZ());
             }
 
             // Mark that the player has spawned for the first time
-            playerData.setBoolean("HasSpawned", true);
+            persistentData.setBoolean("HasSpawned", true);
+            playerMP.getEntityData().setTag(EntityPlayer.PERSISTED_NBT_TAG, persistentData);
+
+            FMLLog.info("[Resurgent Essentials] Player '%s' has joined for the first time and is being teleported to a random location.", playerMP.getName());
         } else {
-            FMLLog.info("[Resurgent Essentials] Player '{}' is rejoining; no random teleportation.", playerMP.getName());
+            FMLLog.info("[Resurgent Essentials] Player '%s' is rejoining; no random teleportation.", playerMP.getName());
         }
     }
-
 
     @SubscribeEvent
     public void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
@@ -46,80 +51,81 @@ public class PlayerRandomSpawnHandler {
 
         EntityPlayerMP playerMP = (EntityPlayerMP) event.player;
 
-        // Log the event to confirm it is being triggered
-        FMLLog.info("[Resurgent Essentials] Player '{}' has respawned.", playerMP.getName());
+        // Log the event
+        FMLLog.info("[Resurgent Essentials] Player '%s' has respawned.", playerMP.getName());
 
-        // Check if random spawning is enabled and the player does not have a bed spawn
-        if (ServerMOTDConfig.isRandomSpawnEnabled() && !hasValidBedSpawn(playerMP)) {
-            FMLLog.info("[Resurgent Essentials] Random spawn is enabled and player '{}' does not have a valid bed spawn. Teleporting to random location.", playerMP.getName());
-
-            // Set the spawn point before the player respawns
+        if (isRandomSpawnAllowed(playerMP)) {
             BlockPos randomSpawnPoint = getRandomSpawnPoint(playerMP.getEntityWorld(), playerMP.getPosition(), ServerMOTDConfig.getSpawnRadius());
 
-            playerMP.connection.setPlayerLocation(randomSpawnPoint.getX() + 0.5, randomSpawnPoint.getY(), randomSpawnPoint.getZ() + 0.5, playerMP.rotationYaw, playerMP.rotationPitch);
-
-            playerMP.attemptTeleport( randomSpawnPoint.getX() + 0.5, randomSpawnPoint.getY(), randomSpawnPoint.getZ() + 0.5);
-
-        } else {
-            FMLLog.info("[Resurgent Essentials] Random spawn is disabled or player '{}' has a valid bed spawn. No random teleportation.", playerMP.getName());
+            // Schedule the teleport to ensure it happens after Minecraft has finished its default spawn handling
+            playerMP.getServerWorld().getMinecraftServer().addScheduledTask(() -> {
+                playerMP.setPositionAndUpdate(randomSpawnPoint.getX() + 0.5, randomSpawnPoint.getY(), randomSpawnPoint.getZ() + 0.5);
+                FMLLog.info("[Resurgent Essentials] Player '%s' successfully teleported to a random location at (%d, %d, %d).", playerMP.getName(), randomSpawnPoint.getX(), randomSpawnPoint.getY(), randomSpawnPoint.getZ());
+            });
         }
     }
 
-
-//    private void setRandomSpawn(EntityPlayerMP player) {
-//        World world = player.getEntityWorld();
-//        int radius = ServerMOTDConfig.getSpawnRadius(); // Get the radius from config
-//        BlockPos spawnPoint = getRandomSpawnPoint(world, player.getPosition(), radius);
-//        player.setPositionAndUpdate(spawnPoint.getX(), spawnPoint.getY(), spawnPoint.getZ());
-//    }
 
     private BlockPos getRandomSpawnPoint(World world, BlockPos center, int radius) {
         Random rand = new Random();
         int x = center.getX() + rand.nextInt(radius * 2) - radius;
         int z = center.getZ() + rand.nextInt(radius * 2) - radius;
 
-        // Get the highest solid or liquid block at the random X and Z position
+        // Find the highest block at the random X and Z position
         BlockPos topBlockPos = world.getTopSolidOrLiquidBlock(new BlockPos(x, 0, z));
-
         int y = topBlockPos.getY(); // Y coordinate of the highest solid or liquid block
 
-        if (world.getBlockState(topBlockPos).getMaterial().isLiquid() || world.getBlockState(topBlockPos).getBlock().isBurning(world, topBlockPos)) {
-            // Retry finding a safe spawn if the top block is liquid or burning
-            return getRandomSpawnPoint(world, center, radius);
+        // Check if the top block is air; if so, find the highest non-air block below
+        while (world.isAirBlock(topBlockPos) && y > 0) {
+            topBlockPos = topBlockPos.down();
+            y = topBlockPos.getY();
         }
 
-        // Ensure a safe height above ground level
-        return new BlockPos(x, y, z);
+        IBlockState blockState = world.getBlockState(topBlockPos);
+
+        // Check if the block is lava or on fire
+        if (blockState.getMaterial().isLiquid()) {
+            if (blockState.getBlock().getUnlocalizedName().equals("tile.lava")
+                    || blockState.getBlock().isBurning(world, topBlockPos)) {
+
+                // If block is lava or on fire, retry to find a safe spawn point
+                return getRandomSpawnPoint(world, center, radius);
+            } else {
+                // If it's water, keep raising block position until it's not liquid to spawn in air
+                do {
+                    topBlockPos = topBlockPos.up();
+                    blockState = world.getBlockState(topBlockPos);
+                } while (blockState.getMaterial().isLiquid() && topBlockPos.getY() < 255.0D);
+
+                return topBlockPos;
+            }
+        }
+
+        // Ensure the player is positioned just above the ground level
+        return new BlockPos(topBlockPos.getX(), topBlockPos.getY() + 1, topBlockPos.getZ());
     }
+
 
     private boolean hasValidBedSpawn(EntityPlayerMP player) {
-        // Get the player's spawn dimension
-        int dimension = player.dimension;
+        // Access the player's persistent NBT data
+        NBTTagCompound playerData = player.getEntityData();
 
-        // Check if the player has a valid world instance
-        World world = player.getEntityWorld();
-        if (world == null) {
-            FMLLog.warning("[Resurgent Essentials] Player '%s' is in an invalid world. Cannot check for a valid bed spawn.", player.getName());
-            return false; // No valid world, cannot have a valid bed spawn
+        // Check if the bed spawn coordinates are set in the NBT data
+        if (playerData.hasKey("SpawnX") && playerData.hasKey("SpawnY") && playerData.hasKey("SpawnZ")) {
+            // Retrieve the bed spawn coordinates
+            int spawnX = playerData.getInteger("SpawnX");
+            int spawnY = playerData.getInteger("SpawnY");
+            int spawnZ = playerData.getInteger("SpawnZ");
+
+            // Confirm that the coordinates are not just default values
+            if (spawnX != 0 || spawnY != 0 || spawnZ != 0) {
+                FMLLog.info("[Resurgent Essentials] Player '%s' has a valid bed spawn set at (%d, %d, %d).", player.getName(), spawnX, spawnY, spawnZ);
+                return true; // A valid bed spawn is set
+            }
         }
 
-        // Get the bed location and check if it exists
-        BlockPos bedLocation = player.getBedLocation(dimension);
-        if (bedLocation == null) {
-            FMLLog.warning("[Resurgent Essentials] Player '%s' does not have a bed location set in dimension %d.", player.getName(), dimension);
-            return false; // No bed location set
-        }
-
-        // Check if the bed location is still valid and safe
-        boolean isSpawnForced = player.isSpawnForced(dimension);
-        BlockPos safeSpawn = EntityPlayerMP.getBedSpawnLocation(world, bedLocation, isSpawnForced);
-        if (safeSpawn == null) {
-            FMLLog.warning("[Resurgent Essentials] Player '%s' has a bed location set in dimension %d, but it is not a valid or safe spawn point.", player.getName(), dimension);
-            return false; // Bed location is not valid or safe
-        }
-
-        // Return true if a valid bed spawn location is found
-        return true;
+        // No valid bed spawn is set
+        FMLLog.info("[Resurgent Essentials] Player '%s' does not have a valid bed spawn set.", player.getName());
+        return false;
     }
-
 }
